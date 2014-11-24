51148b3bbfe267c7704d2c740b4450f9d0f11cdc
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2014-11-24 17:18:36 -0500

    Adding index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function(appId) {
+    return function(request) {
+        request.set('X-Csrf-Token', localStorage['XSRF.Token']);
+        request.set('X-D2L-App-Id', appId);
+        return request;
+    }
+}
