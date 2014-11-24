ddc92335ef44fe4afa53368666c4615ff2f4b3e8
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2014-11-24 18:08:23 -0500

    Switch to requirejs module

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-module.exports = function(appId) {
+define(function() {
+  return function(appId) {
     return function(request) {
-        request.set('X-Csrf-Token', localStorage['XSRF.Token']);
-        request.set('X-D2L-App-Id', appId);
-        return request;
+      request.set('X-Csrf-Token', localStorage['XSRF.Token']);
+      request.set('X-D2L-App-Id', appId);
+      return request;
     }
-}
+  }
+});
