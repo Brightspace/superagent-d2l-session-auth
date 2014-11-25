22350663487e0a799182df1b5917fe0aa3481d3a
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2014-11-25 11:34:47 -0500

    editorconfig reformatting

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 define(function() {
-  return function(appId) {
-    return function(request) {
-      request.set('X-Csrf-Token', localStorage['XSRF.Token']);
-      request.set('X-D2L-App-Id', appId);
-      return request;
-    }
-  }
+	return function(appId) {
+		return function(request) {
+			request.set('X-Csrf-Token', localStorage['XSRF.Token']);
+			request.set('X-D2L-App-Id', appId);
+			return request;
+		}
+	}
 });
