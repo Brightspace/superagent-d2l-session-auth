93c9490cd35340b49989076d2a421d321d69bb32
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2015-02-25 20:01:22 -0500

    Avoid sending the XSRF token cross-origin

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var nock = require('nock'),
+	should = require('should'),
 	request = require('superagent');
 
 var CSRF_TOKEN = 'some-token';
@@ -14,24 +15,30 @@ describe('superagent-valence', function() {
 			.get('/url')
 			.reply(200);
 
-		request.get('http://localhost/url')
+		request.get('/url')
 			.use(valence)
 			.end(function() {});
 
 		endpoint.done();
 	});
 
-	it('adds csrf token', function() {
+	it('adds csrf token for relative URLs', function() {
 		var endpoint = nock('http://localhost')
 			.matchHeader('X-D2L-App-Id', /.*/)
 			.matchHeader('X-Csrf-Token', CSRF_TOKEN)
 			.get('/url')
 			.reply(200);
 
-		request.get('http://localhost/url')
+		request.get('/url')
 			.use(valence)
 			.end(function() {});
 
 		endpoint.done();
+	});
+
+	it('does not add csrf token for non-relative URLs', function() {
+		var req = request.get('http://localhost/url').use(valence);
+
+		should.not.exist(req.header['X-Csrf-Token']);
 	});
 });
